6c860ca8e3a27e08169ec0920f34f6c0988f0093
AugmentedFifth <zcomito@gmail.com>
2017-05-18 03:49:41 -0700

    gOlFeD

--- broken-paths/broken-paths.hs
+++ broken-paths/broken-paths.hs
@@ -1,3 +1,4 @@
+u%w=u!!w
 r=zip[-1,0,1,1,1,0,-1,-1][-1,-1,-1,0,1,1,1,0]
 g _[]_= -1
 g i(e:l)x|x==e=i|1>0=g(i+1)l x
@@ -11,10 +12,10 @@ f n|all(==0).concat.q$n=n|1>0=f$foldr(\p b->s b[k,m]p)n$k!m where
  i!j=[[x,y]|[x,y]<-e,x>i-2&&x<i+2&&y>j-2&&y<j+2]
  q o=[[sum[1|[x,y]<-w!l,let
   u=(x-w,y-l)
-  p=(w-x,l-y)in u/=p&&('1'==(o!!w!!l)!!(r?u))/=('1'==(o!!x!!y)!!(r?p))]|l<-[0..h]]|w<-[0..h]]
- v[x,y][a,b]|q n!!x!!y>q n!!a!!b=[x,y]|1>0=[a,b]
+  p=(w-x,l-y)in u/=p&&('1'==(o%w%l)%(r?u))/=('1'==(o%x%y)%(r?p))]|l<-[0..h]]|w<-[0..h]]
+ v[x,y][a,b]|q n%x%y>q n%a%b=[x,y]|1>0=[a,b]
  [k,m]=foldr1 v e
- s w[x,y][a,b]|x==a&&y==b=w|'1'==w!!x!!y!!(r?(a-x,b-y))=z '0'|'1'==w!!a!!b!!(r?(x-a,y-b))=z '1'|1>0=w where z u=c w x$c(w!!x)y$c(w!!x!!y)(r?(a-x,b-y))u
+ s w[x,y][a,b]|x==a&&y==b=w|'1'==w%x%y%(r?(a-x,b-y))=z '0'|'1'==w%a%b%(r?(x-a,y-b))=z '1'|1>0=w where z u=c w x$c(w%x)y$c(w%x%y)(r?(a-x,b-y))u
 
 
 
