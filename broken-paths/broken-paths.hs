623a000d3cc44f92c17249ff040ecd23b1cd25ca
AugmentedFifth <zcomito@gmail.com>
2017-05-18 01:42:18 -0700

    working example

--- broken-paths/broken-paths.hs
+++ broken-paths/broken-paths.hs
@@ -1,5 +1,5 @@
 main :: IO ()
-main = putStrLn$fixPaths [["00001000","00010000"],["00000100","00000000"]]
+main = print$satisfyMaxImpl [["00001000","00010000"],["00000100","00000000"]]
 
 rotational :: [(Int, Int)]
 rotational=zip[-1,0,1,1,1,0,-1,-1][-1,-1,-1,0,1,1,1,0]
@@ -10,20 +10,21 @@ getIndex i(e:l)x|x==e=i|1>0=getIndex l x i+1
 l?x=getIndex 0 l x
 
 replaceIndex' :: Integral i => i -> [a] -> i -> a -> [a]
-replaceIndex' [] _ _ _ = []
+replaceIndex' _ [] _ _ = []
 replaceIndex' i (e:l) t v
     | i == t = v:l
-    | 1>0    = e:replaceIndex' i+1 l t v
+    | 1>0    = e:replaceIndex' (i+1) l t v
 
 replaceIndex :: Integral i => [a] -> i -> a -> [a]
 replaceIndex = replaceIndex' 0
 
-
+{-
 fixPaths :: [[String]] -> [[String]]
 fixPaths nodes =
     until (\nMx -> all(==0).concat.implicationMx $ nMx) id nodes
+-}
 
-
+implicationMx :: Integral i => [[String]] -> [[i]]
 implicationMx nodes =
     let dim = length nodes-1
         i#j = nodes!!i!!j
@@ -48,9 +49,15 @@ satisfyMaxImpl nodes
             cartesian = mapM(\w->[0..w])[dim,dim]
             maxImpMxCoord[x,y][a,b]|iMx!!x!!y>iMx!!a!!b=[x,y]|1>0=[a,b]
             [maxx,maxy] = foldr1 maxImpMxCoord cartesian
-            locs = [[x,y]|[x,y]<-cartesian,x>i-2&&x<i+2&&y>j-2&&y<j+2]
+            locs = [[x,y]|[x,y]<-cartesian,x>maxx-2&&x<maxx+2&&y>maxy-2&&y<maxy+2]
             fix nodes'[x1,y1][x2,y2]
-                |'1'==nodes'!!x1!!y1!!(rotational?(x2-x1,y2-y1))=replaceIndex
+                |'1'==nodes'!!x1!!y1!!(rotational?(x2-x1,y2-y1))=
+                    replaceIndex nodes' x1 (replaceIndex (nodes'!!x1) y1 (replaceIndex (nodes'!!x1!!y1) (rotational?(x2-x1,y2-y1)) '0'))
+                |'1'==nodes'!!x2!!y2!!(rotational?(x1-x2,y1-y2))=
+                    replaceIndex nodes' x1 (replaceIndex (nodes'!!x1) y1 (replaceIndex (nodes'!!x1!!y1) (rotational?(x2-x1,y2-y1)) '1'))
+                |1>0=
+                    nodes'
+        in  satisfyMaxImpl$foldr (\p accu -> fix accu [maxx,maxy] p) nodes locs
 
 
 {-
